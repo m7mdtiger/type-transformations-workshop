38d5e06be15dfb7712276f125250363e7cf2e662
Tiger <m7mdo_o@yahoo.com>
2023-12-21 14:02:49 -0500

    Add answer to e-08

--- src/02-unions-and-indexing/08-indexed-access.problem.ts
+++ src/02-unions-and-indexing/08-indexed-access.problem.ts
@@ -1,3 +1,5 @@
+import { fakeDataDefaults } from "./08-indexed-access.solution";
+import { fakeDataDefaults } from "./08-indexed-access.problem";
 import { Equal, Expect } from "../helpers/type-utils";
 
 export const fakeDataDefaults = {
@@ -8,16 +10,26 @@ export const fakeDataDefaults = {
   ID: "id",
 };
 
-export type StringType = unknown;
-export type IntType = unknown;
-export type FloatType = unknown;
-export type BooleanType = unknown;
-export type IDType = unknown;
+// export type StringType = unknown;
+// export type IntType = unknown;
+// export type FloatType = unknown;
+// export type BooleanType = unknown;
+// export type IDType = unknown;
+
+/**
+ * Answer
+ */
+type FakeDataTypes = typeof fakeDataDefaults;
+export type StringType = FakeDataTypes["String"];
+export type IntType = FakeDataTypes["Int"];
+export type FloatType = FakeDataTypes["Float"];
+export type BooleanType = FakeDataTypes["Boolean"];
+export type IDType = FakeDataTypes["ID"];
 
 type tests = [
   Expect<Equal<StringType, string>>,
   Expect<Equal<IntType, number>>,
   Expect<Equal<FloatType, number>>,
   Expect<Equal<BooleanType, boolean>>,
-  Expect<Equal<IDType, string>>,
+  Expect<Equal<IDType, string>>
 ];
